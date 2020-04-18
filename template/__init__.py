5761a241960b95518a3ad54bb277d04c7655c76d
pyt3r <pyterbarrows@gmail.com>
2020-04-17 20:11:46 -0400

    enhancing __version__; version bump to 0.0.3

--- template/__init__.py
+++ template/__init__.py
@@ -1,9 +1,14 @@
 def _get_version():
     import os.path
-    from pkg_resources import get_distribution
+    from pkg_resources import get_distribution, DistributionNotFound
     here    = os.path.dirname(__file__)
     package = os.path.basename( here )
-    return get_distribution( package ).version
+
+    try:
+        version = get_distribution( package ).version
+    except DistributionNotFound:
+        version = here
+    return version
 
 
 __version__ = _get_version()
