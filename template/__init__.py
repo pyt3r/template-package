50f8225eec05fa8f2dffbdd0737d4f424726a23a
pyt3r <pyterbarrows@gmail.com>
2020-04-21 23:40:17 -0400

    pep8 fixes

--- template/__init__.py
+++ template/__init__.py
@@ -1,11 +1,11 @@
 def _get_version():
     import os.path
     from pkg_resources import get_distribution, DistributionNotFound
-    here    = os.path.dirname(__file__)
-    package = os.path.basename( here )
+    here = os.path.dirname(__file__)
+    package = os.path.basename(here)
 
     try:
-        version = get_distribution( package ).version
+        version = get_distribution(package).version
     except DistributionNotFound:
         version = here
     return version
