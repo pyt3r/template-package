18b955982ee8fd145484b1272564dec1fa8d9dc8
pyt3r <pyterbarrows@gmail.com>
2020-04-23 00:31:34 -0400

    adding sphinx_rtd_theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,6 +19,8 @@ sys.path.insert(0, os.path.join(here, '..', '..'))
 # import yaml
 # meta = yaml.load(open(os.path.join(here, '..', '..', 'conda-recipe', 'meta.yaml'), 'rb'))
 
+import sphinx_rtd_theme
+
 
 # -- Master document --------------------------------------------------------------
 master_doc = 'index'
@@ -43,7 +45,7 @@ release = '0.0.4dev'
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
-    "sphinx_rtd_theme",
+    'sphinx_rtd_theme',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -60,11 +62,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
-
-html_theme_options = {
-    'collapse_navigation': True,
-}
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
